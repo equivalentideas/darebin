cf446f9cc9add38a3efd3c115cd8d6ee37880c82
Luke Bacon <luke@equivalentideas.com>
2015-06-17 18:22:51 +1000

    Set the verify mode on the agent to not check the certificate
    This gets the scraper to work, probably because the council's
    certificate settings are out of date according chrome.

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,10 @@ require 'date'
 base_url = "https://eservices.darebin.vic.gov.au/ePathway/Production/Web/generalenquiry/"
 url = "#{base_url}enquirylists.aspx"
 
-agent = Mechanize.new
+agent = Mechanize.new do |a|
+    a.verify_mode = OpenSSL::SSL::VERIFY_NONE
+end
+
 first_page = agent.get url
 p first_page.title.strip
 first_page_form = first_page.forms.first
